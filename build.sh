04acbefeb33f2f349d789178d5a33f9548055e80
Csaba Maulis <csaba.maulis@gmail.com>
2015-11-27 12:56:38 +0100

    Refactoring build script

--- build.sh
+++ build.sh
@@ -4,21 +4,59 @@
 #
 # The MIT License (MIT)
 
-echo "+ mkdir dist"
-mkdir dist
-echo "+ rm dist/*.box"
-rm dist/*.box
-echo "+ vagrant up"
-vagrant up
-echo "+ vagrant halt"
-vagrant halt
-echo "+ vagrant package precise --output dist/precise.box"
-vagrant package precise --output dist/precise.box
-echo "+ vagrant package trusty --output dist/trusty.box"
-vagrant package trusty --output dist/trusty.box
-echo "+ vagrant box add src/precise.json"
-vagrant box add src/precise.json
-echo "+ vagrant box add src/trusty.json"
-vagrant box add src/trusty.json
-echo "+ rm dist/*.box"
-rm dist/*.box
+do_build() {
+    if [ ! -d "dist" ]
+        then
+        mkdir dist
+    fi
+    if [ -f "dist/${BOX_NAME}.box" ]
+        then
+        rm dist/${BOX_NAME}.box
+    fi
+    vagrant up ${BOX_NAME}
+    vagrant provision ${BOX_NAME}
+    vagrant package ${BOX_NAME} --output dist/${BOX_NAME}.box
+    vagrant box add src/${BOX_NAME}.json
+    rm dist/${BOX_NAME}.box
+}
+
+do_help() {
+    echo "Argument missing or invalid!"
+    echo ""
+    echo "Usage: build.sh <subcommand>"
+    echo ""
+    echo "Available subcommands:"
+    echo "    all            Building all vagrant boxes"
+    echo "    precise        Building 'precise64' vagrant box"
+    echo "    trusty         Building 'trusty64' vagrant box"
+    echo ""
+}
+
+set -e
+
+if [ $# -eq 0 ]
+  then
+    do_help
+    exit
+fi
+
+if [ $1 == "all" ]
+  then
+    echo "    Building all vagrant boxes"
+    BOX_NAME="precise"
+    do_build
+    BOX_NAME="trusty"
+    do_build
+elif [ $1 == "precise" ]
+  then
+    echo "    Building 'precise' vagrant box"
+    BOX_NAME="precise"
+    do_build
+elif [ $1 == "trusty" ]
+    then
+    echo "    Building 'trusty' vagrant box"
+    BOX_NAME="trusty"
+    do_build
+else
+    do_help
+fi
