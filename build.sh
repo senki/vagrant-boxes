2c263c434835b30283c2b3a34491bcc26c342bcb
Csaba Maulis <csaba.maulis@gmail.com>
2015-12-19 18:34:46 +0100

    DB files not needed

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ source src/logcheck.sh
 
 do_build() {
     echo -e "${GREEN}Building ubuntu ${BOX_NAME} tls x64 vagrant box${NC}"
-    find ./vagrant/db -type f \( ! -iname "*.gitignore" \) -mtime +1 -delete
+    find ./vagrant/db -type f \( ! -iname "*.gitignore" \) -delete
     set +e
     vagrant box remove senki/$(echo ${BOX_NAME//_/-}) -f
     set -e
