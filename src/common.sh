fb5c420bf00b8292e0eafef49907a39925a0ead1
Csaba Maulis <csaba.maulis@gmail.com>
2016-04-24 14:13:18 +0200

    Update deps

--- src/common.sh
+++ src/common.sh
@@ -11,7 +11,7 @@
 HOST_CONFIG="/etc/hosts"
 LOCALE_CONFIG="/etc/default/locale"
 MULTITAIL_CONFIG="/etc/multitail.conf"
-VBOX_GA_VERS="5.0.16"
+VBOX_GA_VERS="5.0.18"
 
 MYSQL_ROOT_PASS="vagrant"
 PROVISION_LOG="/var/log/provision.log"
